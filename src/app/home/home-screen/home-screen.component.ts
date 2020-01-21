f9b9c4fbc3ab9baf384b67e140de02d2e164ccb0
TodoreTu <TodoreTu>
2020-01-21 19:37:51 +0200

    progress

---
+++ src/app/home/home-screen/home-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-home-screen',
+  templateUrl: './home-screen.component.html',
+  styleUrls: ['./home-screen.component.scss']
+})
+export class HomeScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
