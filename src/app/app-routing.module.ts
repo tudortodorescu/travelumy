307ca32f609655c989f837fbe4e1cf335d014c83
TodoreTu <TodoreTu>
2020-01-22 03:01:10 +0200

    added the login / register components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 import { HomeScreenComponent } from "./home/home-screen/home-screen.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
 
 const routes: Routes = [
-    { path: "", component: HomeScreenComponent }
+    { path: "", component: HomeScreenComponent },
+    { path: "/auth-login", component: LoginComponent },
+    { path: "/auth-register", component: RegisterComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeScreenComponent } from './home/home-screen/home-screen.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
     imports: [
         NativeScriptModule,
         AppRoutingModule
     ],
-    declarations: [AppComponent, HomeScreenComponent],
+    declarations: [AppComponent, HomeScreenComponent, LoginComponent, RegisterComponent],
     schemas: [NO_ERRORS_SCHEMA],
     bootstrap: [AppComponent]
 })
